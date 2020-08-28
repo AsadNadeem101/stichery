5cf0c4d1eaa1f5f35e6b4b64bf42f6eed3cf0c0c
iotcloudware <iotcloudware@gmail.com>
2020-08-28 22:29:16 +0500

    asad pull

---
+++ app/Http/Controllers/testController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class testController extends Controller
+{
+    // public function index()
+    // {
+    //  //    foreach ($month_array as $key => $value) 
+	   //  // {
+	   //    if(1==1)
+	   //    {
+	   //      $day=1;
+	   //      $hour=5;
+	   //      $date=2020-01-01;
+
+	   //      while ($day <= 31) 
+	   //      {
+	   //          //store procedure...........
+	   //          // $data=new Data;
+	   //          // $data->time=$date;
+	   //          // $data->save();
+
+	   //          $date=Carbon::parseaddHours($hour);
+	   //          $day=Carbon::parse($date)->format('d');  //date carbon
+
+	   //          \Log::info($day);
+
+	   //      }
+	          
+	   //     }
+	   //     elseif($value=='Feb')
+	   //     {
+	      
+	   //      }
+	   //  // }
+    // }
+}
